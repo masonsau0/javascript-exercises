5335176600f3fe2689f541caf7d2ea1ac24ecc5c
masonsau0 <masonsau0@gmail.com>
2022-05-23 00:30:02 -0400

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius;
+  celcius = ((temp-32) * 5) / 9;
+  return  Number(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit;
+  fahrenheit = 1.8 * temp + 32;
+  return  Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
