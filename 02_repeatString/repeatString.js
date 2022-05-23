ef84549b4480810ee292decb65225d4753381051
masonsau0 <masonsau0@gmail.com>
2022-05-22 23:22:22 -0400

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 1; i <= num; i++) {
+       word = word + string;
+    }
+    return word;
 };
 
 // Do not edit below this line
